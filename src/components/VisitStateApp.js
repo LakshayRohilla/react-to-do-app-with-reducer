208d06ce50c8df897d57af9de6ae197b80fa1c50
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2023-04-26 10:44:52 +0530

    Final code dispatch issue

---
+++ src/components/VisitStateApp.js
@@ -0,0 +1,10 @@
+export default function VisitStateApp() {
+
+    return (
+        <a href="https://github.com/LakshayRohilla/react-to-do-with-state" target="_blank" rel="noopener noreferrer">
+            <button>
+                Go to App using useState
+            </button>
+        </a>
+    )
+}
